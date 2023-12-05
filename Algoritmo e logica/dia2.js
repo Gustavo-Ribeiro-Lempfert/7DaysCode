e3ffd13982505efc2c50237d333427c3bd7f2022
Gustavo Lemfpert <gustavoribe00@outlook.com>
2023-12-05 05:26:58 -0300

    Solução 2° dia logica de programação

---
+++ Algoritmo e logica/dia2.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual seu nome?")
+const idade = prompt("Quantos anos você tem?")
+const linguagem= prompt("Qual linguagem de programação você estuda?")
+const msg = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`
+
+alert(msg)
